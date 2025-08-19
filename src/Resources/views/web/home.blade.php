1affccf99327a27726164a36f62eaebbd9700bb7
Michael Bishop <michael@bishop.net.za>
2025-08-19 14:27:53 +0200

    Images for home page

--- src/Resources/views/web/home.blade.php
+++ src/Resources/views/web/home.blade.php
@@ -1,8 +1,14 @@
 <x-lightworx::layouts.web pageName="Home">
     <div class="text-center my-5">
         <img class="pt-5 img-fluid" src="{{asset('/lightworx/images/lightworx.png')}}">
-        <h3>Creative business solutions</h3>
-        <h3>using <a style="text-decoration:none;" target="_blank" href="https://www.laravel.com">Laravel</a> and <a style="text-decoration:none;" target="_blank" href="https://www.filamentphp.com">Filament</a></h3>
-        <h5 class="pt-5"><a style="text-decoration:none;" class="btn btn-primary" href="mailto:michael@lightworx.co.za">Contact us</a></h5>
+        <h3>Open source systems and solutions</h3>
+        <h5 class="pt-5"><a style="text-decoration:none;background-color:#f9e258;color:black;" class="btn" href="mailto:michael@lightworx.co.za">Contact us</a></h5>
+        <a style="text-decoration:none;" target="_blank" href="https://www.laravel.com" title="Laravel">
+            <img style="width:50px;" class="pt-5" src="{{asset('/lightworx/images/laravel.jpg')}}">
+        </a> 
+        &nbsp;&nbsp;
+        <a style="text-decoration:none;" target="_blank" href="https://www.filamentphp.com" title="Filament">
+            <img style="width:50px;" class="pt-5" src="{{asset('/lightworx/images/filament.jpg')}}">
+        </a>
     </div>
 </x-lightworx::layouts.web>
