da1b0b8ef514816c5c2af5a907a8c5954f12f570
Michael Bishop <michael@bishop.net.za>
2025-08-19 11:27:57 +0200

    Home page header

--- src/Resources/views/web/home.blade.php
+++ src/Resources/views/web/home.blade.php
@@ -2,7 +2,7 @@
     <div class="text-center my-5">
         <img class="pt-5 img-fluid" src="{{asset('/lightworx/images/lightworx.png')}}">
         <h3>Creative business solutions</h3>
-        <h4>using <a style="text-decoration:none;" target="_blank" href="https://www.laravel.com">Laravel</a> and <a style="text-decoration:none;" target="_blank" href="https://www.filamentphp.com">Filament</a></h4>
+        <h3>using <a style="text-decoration:none;" target="_blank" href="https://www.laravel.com">Laravel</a> and <a style="text-decoration:none;" target="_blank" href="https://www.filamentphp.com">Filament</a></h3>
         <h5 class="pt-5"><a style="text-decoration:none;" href="mailto:michael@lightworx.co.za">Contact us</a></h5>
     </div>
 </x-lightworx::layouts.web>
