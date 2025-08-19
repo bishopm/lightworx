1813a6b144deae9821315f9317baae5f3cc2c014
Michael Bishop <michael@bishop.net.za>
2025-08-19 11:23:26 +0200

    Image resizing

--- src/Resources/views/web/home.blade.php
+++ src/Resources/views/web/home.blade.php
@@ -1,6 +1,6 @@
 <x-lightworx::layouts.web pageName="Home">
     <div class="text-center my-5">
-        <img class="pt-5" src="{{asset('/lightworx/images/lightworx.png')}}">
+        <img class="pt-5 img-fluid" src="{{asset('/lightworx/images/lightworx.png')}}">
         <h3>Creative business solutions</h3>
         <h4>using <a style="text-decoration:none;" target="_blank" href="https://www.laravel.com">Laravel</a> and <a style="text-decoration:none;" target="_blank" href="https://www.filamentphp.com">Filament</a></h4>
         <h5 class="pt-5"><a style="text-decoration:none;" href="mailto:michael@lightworx.co.za">Contact us</a></h5>
