60501041cd03ad5a25419aeea194177451e72b06
Michael Bishop <michael@bishop.net.za>
2025-08-19 11:36:03 +0200

    Contact button

--- src/Resources/views/web/home.blade.php
+++ src/Resources/views/web/home.blade.php
@@ -3,6 +3,6 @@
         <img class="pt-5 img-fluid" src="{{asset('/lightworx/images/lightworx.png')}}">
         <h3>Creative business solutions</h3>
         <h3>using <a style="text-decoration:none;" target="_blank" href="https://www.laravel.com">Laravel</a> and <a style="text-decoration:none;" target="_blank" href="https://www.filamentphp.com">Filament</a></h3>
-        <h5 class="pt-5"><a style="text-decoration:none;" href="mailto:michael@lightworx.co.za">Contact us</a></h5>
+        <h5 class="pt-5"><a style="text-decoration:none;" class="btn btn-primary" href="mailto:michael@lightworx.co.za">Contact us</a></h5>
     </div>
 </x-lightworx::layouts.web>
