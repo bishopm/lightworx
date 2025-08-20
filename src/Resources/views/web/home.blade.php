71c547dca68f165b5a655daba17d3fb40ae9e10b
Michael Bishop <michael@bishop.net.za>
2025-08-20 08:39:49 +0200

    Remove extra images

--- src/Resources/views/web/home.blade.php
+++ src/Resources/views/web/home.blade.php
@@ -1,14 +1,7 @@
 <x-lightworx::layouts.web pageName="Home">
-    <div class="text-center my-5">
+    <div class="text-center">
         <img class="pt-5 img-fluid" src="{{asset('/lightworx/images/lightworx.png')}}">
         <h3>Open source systems and solutions</h3>
         <h5 class="pt-5"><a style="text-decoration:none;background-color:#f9e258;color:black;" class="btn" href="mailto:michael@lightworx.co.za">Contact us</a></h5>
-        <a style="text-decoration:none;" target="_blank" href="https://www.laravel.com" title="Laravel">
-            <img style="width:50px;" class="pt-5" src="{{asset('/lightworx/images/laravel.jpg')}}">
-        </a> 
-        &nbsp;&nbsp;
-        <a style="text-decoration:none;" target="_blank" href="https://www.filamentphp.com" title="Filament">
-            <img style="width:50px;" class="pt-5" src="{{asset('/lightworx/images/filament.jpg')}}">
-        </a>
     </div>
 </x-lightworx::layouts.web>
